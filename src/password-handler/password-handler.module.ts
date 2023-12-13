d0e95f8079ce07d97f3a506449659cd9511fc699
unknown <arsanyjoseph@gmail.com>
2023-12-14 01:58:57 +0200

    exports PasswordHandlerService class

--- src/password-handler/password-handler.module.ts
+++ src/password-handler/password-handler.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { PasswordHandlerService } from "./password-handler.service";
 
 @Module({
+  exports: [PasswordHandlerModule],
   providers: [PasswordHandlerService],
 })
 export class PasswordHandlerModule {}
