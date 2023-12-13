c16649c713d99df1add71a5716d28ebf4a229e9b
unknown <arsanyjoseph@gmail.com>
2023-12-14 00:38:31 +0200

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+import { UserModule } from "./user/user.module";
+import { typeOrmConfig } from "./config/typeOrm.config";
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
