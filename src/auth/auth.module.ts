c7312e683fdb688f266cf7c92432cbb1cf2bf36a
unknown <arsanyjoseph@gmail.com>
2023-12-14 13:10:13 +0200

    sort imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { APP_GUARD } from "@nestjs/core";
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
@@ -7,7 +8,6 @@ import { LocalStrategy } from "./strategy/local.strategy";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
 import { UserModule } from "src/user/user.module";
 import { jwtConfig } from "src/config/jwt.config";
-import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { JwtStrategy } from "./strategy/jwt.startegy";
 
