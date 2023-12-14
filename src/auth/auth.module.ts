07db93cfe605b967dc7fed3141aede03805d6a96
unknown <arsanyjoseph@gmail.com>
2023-12-14 20:50:23 +0200

    register RolesGuard gloablly

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,6 +10,7 @@ import { UserModule } from "src/user/user.module";
 import { jwtConfig } from "src/config/jwt.config";
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { JwtStrategy } from "./strategy/jwt.startegy";
+import { RolesGuard } from "./guards/user-roles.guard";
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.registerAsync(jwtConfig)],
@@ -19,6 +20,10 @@ import { JwtStrategy } from "./strategy/jwt.startegy";
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
     JwtStrategy,
     PasswordHandlerService,
