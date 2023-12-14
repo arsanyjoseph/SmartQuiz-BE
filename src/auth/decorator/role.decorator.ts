69b48b52e5b3d29af672c82153430db003f01b0b
unknown <arsanyjoseph@gmail.com>
2023-12-14 14:07:15 +0200

    move decorators to auth directory

--- src/auth/decorator/role.decorator.ts
+++ src/auth/decorator/role.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from "@nestjs/common";
-import { UserRoles } from "../user/types/user.enum";
+import { UserRoles } from "../../user/types/user.enum";
 
 export const ROLES_KEY = "roles";
 export const Roles = (...roles: UserRoles[]) => SetMetadata(ROLES_KEY, roles);
