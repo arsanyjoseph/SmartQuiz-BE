d33fbddd46568ec4505b4564b636252642e0e90b
unknown <arsanyjoseph@gmail.com>
2023-12-14 10:22:15 +0200

    register Passport Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,16 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
+import { LocalStrategy } from "./strategy/local.strategy";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
 import { UserModule } from "src/user/user.module";
 import { jwtConfig } from "src/config/jwt.config";
 
 @Module({
-  imports: [UserModule, JwtModule.registerAsync(jwtConfig)],
-  providers: [AuthService, PasswordHandlerService],
+  imports: [UserModule, PassportModule, JwtModule.registerAsync(jwtConfig)],
+  providers: [AuthService, LocalStrategy, PasswordHandlerService],
   controllers: [AuthController],
 })
 export class AuthModule {}
