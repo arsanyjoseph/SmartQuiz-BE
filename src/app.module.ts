13f1bfc1f352c5b3de5d2f4df54aa6724f2b932f
unknown <arsanyjoseph@gmail.com>
2023-12-14 02:00:52 +0200

    prettier fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { ConfigModule } from "@nestjs/config";
 
 import { UserModule } from "./user/user.module";
 import { typeOrmConfig } from "./config/typeOrm.config";
+import { AuthModule } from "./auth/auth.module";
+import { PasswordHandlerModule } from "./password-handler/password-handler.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(typeOrmConfig),
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    AuthModule,
+    PasswordHandlerModule,
   ],
   controllers: [],
   providers: [],
