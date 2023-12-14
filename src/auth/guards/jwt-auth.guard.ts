ff34d0142f2d182e1498b9644cfa96028cd6a34d
unknown <arsanyjoseph@gmail.com>
2023-12-14 14:08:15 +0200

    fix import & add role to jwt

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,7 +1,7 @@
 import { ExecutionContext, Injectable } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
-import { IS_PUBLIC_KEY } from "src/utils/public.decorator";
+import { IS_PUBLIC_KEY } from "src/auth/decorator/public.decorator";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
