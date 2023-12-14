b3211ada613ef6cb6affabf76f8501ea3c2760e6
unknown <arsanyjoseph@gmail.com>
2023-12-14 02:49:31 +0200

    add initial logic of login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import {
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { AuthDto } from "./dto/auth.dto";
 import { UserRepository } from "src/user/user.repository";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
@@ -6,10 +10,22 @@ import { PasswordHandlerService } from "src/password-handler/password-handler.se
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly usersRepository: UserRepository,
+    private readonly userRepository: UserRepository,
     private readonly passwordHandler: PasswordHandlerService,
   ) {}
   async login(authDto: AuthDto) {
+    const user = await this.userRepository.getUserByEmail(authDto.email);
+    if (!user) {
+      throw new HttpException("User not found", 404);
+    }
+    const isPasswordVerified = this.passwordHandler.verifyPassword(
+      user.password,
+      authDto.password,
+    );
+
+    if (!isPasswordVerified) {
+      throw new UnauthorizedException("Password is not matching");
+    }
     console.log(authDto);
   }
 }
