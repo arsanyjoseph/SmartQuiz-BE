75f4684473f4d347cfa8753763bc9a2ee9a27d1e
unknown <arsanyjoseph@gmail.com>
2023-12-14 03:07:01 +0200

    sort imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
 import { UserModule } from "src/user/user.module";
-import { JwtModule } from "@nestjs/jwt";
 import { jwtConfig } from "src/config/jwt.config";
 
 @Module({
