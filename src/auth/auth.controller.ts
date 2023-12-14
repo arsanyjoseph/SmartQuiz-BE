a9e1bee986fddbd2bce78443901e3b2911f6cb2e
unknown <arsanyjoseph@gmail.com>
2023-12-14 11:15:17 +0200

    implment @Public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Post, UseGuards, HttpCode, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { User } from "src/user/user.entity";
+import { Public } from "src/utils/public.decorator";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @HttpCode(200)
   @Post("/login")
