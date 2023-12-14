a15c7487fe7ada98fdfc18a69d51ef4cb772703c
unknown <arsanyjoseph@gmail.com>
2023-12-14 02:00:27 +0200

    fix dto name

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { UserDto } from "./dto/user.dto";
+import { CreateUserDto } from "./dto/user.dto";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post("/register")
-  async register(@Body() user: UserDto) {
+  async register(@Body() user: CreateUserDto) {
     return await this.userService.register(user);
   }
 }
