f00861fa5ac2db4bedc0beb842a489a5363420fa
unknown <arsanyjoseph@gmail.com>
2023-12-14 03:05:27 +0200

    register jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
 import { UserModule } from "src/user/user.module";
+import { JwtModule } from "@nestjs/jwt";
+import { jwtConfig } from "src/config/jwt.config";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.registerAsync(jwtConfig)],
   providers: [AuthService, PasswordHandlerService],
   controllers: [AuthController],
 })
