99cb2c7ac6285dfa77f246cb7496075a3fc636d1
unknown <arsanyjoseph@gmail.com>
2023-12-14 11:14:16 +0200

    register JwtAuthGuard as Global for all routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,25 @@ import { PassportModule } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { LocalStrategy } from "./strategy/local.strategy";
-import { JwtStrategy } from "./strategy/jwt.startegy";
 import { PasswordHandlerService } from "src/password-handler/password-handler.service";
 import { UserModule } from "src/user/user.module";
 import { jwtConfig } from "src/config/jwt.config";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
+import { JwtStrategy } from "./strategy/jwt.startegy";
 
 @Module({
   imports: [UserModule, PassportModule, JwtModule.registerAsync(jwtConfig)],
-  providers: [AuthService, LocalStrategy, JwtStrategy, PasswordHandlerService],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    JwtStrategy,
+    PasswordHandlerService,
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
