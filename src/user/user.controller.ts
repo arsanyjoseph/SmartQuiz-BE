62f39ec74a9f85b9d357afd2087a992edc028b3e
unknown <arsanyjoseph@gmail.com>
2023-12-14 20:52:54 +0200

    call getProfile fn from UserService

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/user.dto";
+import { Public } from "src/auth/decorator/public.decorator";
 
 @Controller("user")
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Post("/register")
+  @Public()
+  @Post("register")
   async register(@Body() createUserDto: CreateUserDto) {
     return await this.userService.register(createUserDto);
+  }
+  @Get("me")
+  async getProfile(@Req() { user }: { user: { id: string; email: string } }) {
+    return await this.userService.getProfile(user.email);
   }
 }
